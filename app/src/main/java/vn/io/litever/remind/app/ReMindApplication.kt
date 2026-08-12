c965402e1de2ccfac0e1ca41df116e5c7a7212cf
Trần Đức Toàn <toantd2000@gmail.com>
2026-08-12 10:57:27 +0700

    build: remove legacy code-based firebase analytics check

--- app/src/main/java/vn/io/litever/remind/app/ReMindApplication.kt
+++ app/src/main/java/vn/io/litever/remind/app/ReMindApplication.kt
@@ -4,17 +4,10 @@ import android.app.Application
 
 import dagger.hilt.android.HiltAndroidApp
 
-import com.google.firebase.analytics.FirebaseAnalytics
-import vn.io.litever.remind.app.BuildConfig
-
 @HiltAndroidApp
 class ReMindApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        if (BuildConfig.DEBUG) {
-            FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(false)
-        }
     }
 }
 
